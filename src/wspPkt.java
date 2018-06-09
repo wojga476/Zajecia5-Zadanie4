0bb9655c2e64267dcb37bb1f87e9f38559612fc0
wojga476 <wojga476@gmail.com>
2018-06-09 15:41:07 +0200

    poprawa-wyświetlenie informacji o współrzędnych punktu

--- src/wspPkt.java
+++ src/wspPkt.java
@@ -10,35 +10,19 @@ public class wspPkt {
         int wspY = wspol.nextInt();
         wspol.close();
 
-        if (wspX > 0) {
 
-            while (wspY > 0) {
-                System.out.printf(("Punkt (%d,%d) leży w I ćwiartce układu współrzędnych."), wspX, wspY);
-                break;
-
-            }
-            while (wspY < 0) {
-
-                System.out.printf(("Punkt (%d,%d) leży w IV ćwiartce układu współrzędnych."), wspX, wspY);
-                break;
-            }
-
-        } else {
-
-            while (wspY > 0) {
-
-                System.out.printf(("Punkt (%d,%d) leży w II ćwiartce układu współrzędnych."), wspX, wspY);
-                break;
-            }
-
-            while (wspY < 0) {
-
-                System.out.printf(("Punkt (%d,%d) leży w III ćwiartce układu współrzędnych."), wspX, wspY);
-                break;
-            }
-        }
-        if (wspX == 0 && wspY == 0) {
+        if (wspX > 0 && wspY > 0) {
+            System.out.printf(("Punkt (%d,%d) leży w I ćwiartce układu współrzędnych."), wspX, wspY);
+        } else if (wspX < 0 && wspY > 0) {
+            System.out.printf(("Punkt (%d,%d) leży w II ćwiartce układu współrzędnych."), wspX, wspY);
+        } else if (wspX > 0 && wspY < 0) {
+            System.out.printf(("Punkt (%d,%d) leży w IV ćwiartce układu współrzędnych."), wspX, wspY);
+        } else if (wspX < 0 && wspY < 0) {
+            System.out.printf(("Punkt (%d,%d) leży w III ćwiartce układu współrzędnych."), wspX, wspY);
+        } else if (wspX == 0 && wspY == 0) {
             System.out.printf(("Punkt (%d,%d) leży na początku układu współrzędnych."), wspX, wspY);
         }
+
     }
 }
+
